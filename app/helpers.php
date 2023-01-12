7907326fb409175fbd1951e76cfd71acc987f133
gyp719 <836230540@qq.com>
2023-01-12 18:55:46 +0800

    助手函数

--- app/helpers.php
+++ app/helpers.php
@@ -67,3 +67,60 @@ function getSlicedStrArray($str, int $groupSize = 8000, string $encoding = 'UTF-
 
     return $strArray;
 }
+
+
+/**
+ * 对富文本信息中的数据
+ * 匹配出所有的 <img> 标签的 src属性
+ * @param  string  $contentStr  富文本字符串
+ * @return array
+ */
+function getPatternMatchImages(string $contentStr = ""): array
+{
+    $imgSrcArr = array();
+    //首先将富文本字符串中的 img 标签进行匹配
+    $pattern_imgTag = '/<img\b.*?(?:\>|\/>)/i';
+    preg_match_all($pattern_imgTag, $contentStr, $matchIMG);
+    if (isset($matchIMG[0])) {
+        foreach ($matchIMG[0] as $key => $imgTag) {
+            //进一步提取 img标签中的 src属性信息
+            $pattern_src = '/\bsrc\b\s*=\s*[\'\"]?([^\'\"]*)[\'\"]?/i';
+            preg_match_all($pattern_src, $imgTag, $matchSrc);
+            if (isset($matchSrc[1])) {
+                foreach ($matchSrc[1] as $src) {
+                    //将匹配到的src信息压入数组
+                    $imgSrcArr[] = $src;
+                }
+            }
+        }
+    }
+    return $imgSrcArr;
+}
+
+
+/**
+ * 获取多维数组某个特定键的所有值
+ * @param  string  $key 要查找的 key
+ * @param  array  $array 在其中查找的数组。
+ * @param string $default 如果 key 不存在，则返回的默认值。
+ * @return mixed|null
+ */
+function getArrKeyValue(string $key, array $array, string $default = '')
+{
+    if (isset($array[$key])) {
+        return $array[$key];
+    }
+    foreach ($array as $value) {
+        if (is_object($value)) {
+            $value = (array)$value;
+        }
+        if (is_array($value)) {
+            $result = getArrKeyValue($key, $value, $default);
+            if ($result !== $default) {
+                return $result;
+            }
+        }
+    }
+
+    return $default;
+}
